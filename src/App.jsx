9b7b4ab90972a315be384696ee71566b3b4fc011
a-nester <a_nester@ukr.net>
2024-06-21 23:08:04 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
 import "./App.css";
+import { useEffect } from "react";
 import { ContactForm } from "./components/ContactForm/ContactForm";
 import { ContactList } from "./components/ContactList/ContactList";
 import { SearchBox } from "./components/SearchBox/SearchBox";
