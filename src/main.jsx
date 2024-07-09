d1d76483a58c36114ace759c3e19645bc0543bd5
a-nester <a_nester@ukr.net>
2024-07-09 12:26:33 +0300

    del-comments

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,11 @@ import "./index.css";
 import "modern-normalize";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-// import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
       <App />
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
